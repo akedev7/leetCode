ed4fe7546b911af7cca913a872da1d3f59673009
ake.tan <ake.tan@ascendcorp.com>
2022-07-21 09:56:19 +0700

    Maze problem with Out of bound checking but no back tracking yet.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 
+import javax.imageio.ImageReader;
 import java.util.Stack;
 
 public class Main {
@@ -11,12 +12,12 @@ public class Main {
         //0 wall
         //2 destination
         int[][] maze = {
-                {2, 0, 0, 1},
+                {1, 0, 0, 0},
                 {1, 1, 1, 1},
-                {0, 0, 0, 0}
+                {0, 0, 0, 2}
         };
 
-        Position startingPosition = new Position(0, 3);
+        Position startingPosition = new Position(0, 0);
 
         Stack<Position> paths = new Stack<>();
         paths.push(startingPosition);
@@ -25,47 +26,59 @@ public class Main {
             maze[peekPosition.x][peekPosition.y] = 0;
 
             //Check bottom
-            int bottomDirection = maze[peekPosition.x + 1][peekPosition.y];
-            if (bottomDirection == 2) {
-                System.out.println("Go down, and you won");
-                break;
-            } else if (bottomDirection == 1) {
-                System.out.println("Go down");
-                paths.push(new Position(peekPosition.x + 1, peekPosition.y));
-                continue;
+            int xBottom = peekPosition.x + 1;
+            if (xBottom < maze.length) {
+                int bottomDirection = maze[xBottom][peekPosition.y];
+                if (bottomDirection == 2) {
+                    System.out.println("Go down, and you won");
+                    break;
+                } else if (bottomDirection == 1) {
+                    System.out.println("Go down");
+                    paths.push(new Position(xBottom, peekPosition.y));
+                    continue;
+                }
             }
 
             //Check Top
-            int upDirection = maze[peekPosition.x - 1][peekPosition.y];
-            if (upDirection == 2) {
-                System.out.println("Go top, and you won");
-                break;
-            } else if (upDirection == 1) {
-                System.out.println("Go top");
-                paths.push(new Position(peekPosition.x - 1, peekPosition.y));
-                continue;
+            int xTop = peekPosition.x - 1;
+            if (xTop >= 0) {
+                int upDirection = maze[xTop][peekPosition.y];
+                if (upDirection == 2) {
+                    System.out.println("Go top, and you won");
+                    break;
+                } else if (upDirection == 1) {
+                    System.out.println("Go top");
+                    paths.push(new Position(xTop, peekPosition.y));
+                    continue;
+                }
             }
 
             //Check Left
-            int leftDirection = maze[peekPosition.x][peekPosition.y - 1];
-            if (leftDirection == 2) {
-                System.out.println("Go left, and you won");
-                break;
-            } else if (leftDirection == 1) {
-                System.out.println("Go left");
-                paths.push(new Position(peekPosition.x, peekPosition.y - 1));
-                continue;
+            int yLeft = peekPosition.y - 1;
+            if (yLeft >= 0) {
+                int leftDirection = maze[peekPosition.x][yLeft];
+                if (leftDirection == 2) {
+                    System.out.println("Go left, and you won");
+                    break;
+                } else if (leftDirection == 1) {
+                    System.out.println("Go left");
+                    paths.push(new Position(peekPosition.x, yLeft));
+                    continue;
+                }
             }
 
             //Check right
-            int rightDirection = maze[peekPosition.x][peekPosition.y + 1];
-            if (rightDirection == 2) {
-                System.out.println("Go right, and you won");
-                break;
-            } else if (rightDirection == 1) {
-                System.out.println("Go right");
-                paths.push(new Position(peekPosition.x, peekPosition.y + 1));
-                continue;
+            int yRight = peekPosition.y + 1;
+            if (yRight < maze[peekPosition.x].length) {
+                int rightDirection = maze[peekPosition.x][yRight];
+                if (rightDirection == 2) {
+                    System.out.println("Go right, and you won");
+                    break;
+                } else if (rightDirection == 1) {
+                    System.out.println("Go right");
+                    paths.push(new Position(peekPosition.x, yRight));
+                    continue;
+                }
             }
         }
     }
